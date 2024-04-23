52ba5270aafaf240333093de590bb2747cd274b4
mstsurnalyakter <surnalyms@gmail.com>
2024-04-23 07:01:02 +0600

    create context api

---
+++ src/pages/SgnIn/SgnIn.jsx
@@ -0,0 +1,9 @@
+const SgnIn = () => {
+  return (
+    <div>
+        <h2>Please Sign In</h2>
+    </div>
+  )
+}
+
+export default SgnIn
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,8 @@ import Root from "../layout/Root";
 import Home from "../pages/Home/Home";
 import AddCoffee from "../pages/AddCoffee/AddCoffee";
 import UpdateCoffee from "../pages/UpdateCoffee/UpdateCoffee";
+import SignUp from "../pages/SignUp/SignUp";
+import SgnIn from "../pages/SgnIn/SgnIn";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +25,14 @@ const router = createBrowserRouter([
         element: <UpdateCoffee />,
         loader: ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`),
       },
+      {
+        path:"/signUp",
+        element:<SignUp/>
+      },
+      {
+        path:"/signIn",
+        element:<SgnIn/>
+      }
     ],
   },
 ]);
